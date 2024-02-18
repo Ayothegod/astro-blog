8916df6e3cf50c4826dff6d344858f8d26ff946d
Ayo <heyayomideadebisi@gmail.com>
2024-02-18 23:21:46 +0100

    feat: create custom link component

---
+++ src/data/navData.ts
@@ -0,0 +1,17 @@
+interface NavItem {
+    name: String,
+    path: String
+} 
+
+const navData: NavItem[] = [
+    {
+        name: "About",
+        path: "/about/",
+    },
+    {
+        name: "Blog",
+        path: "/blog/",
+    },
+]
+
+export default navData
